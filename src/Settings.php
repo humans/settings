7187a7af122757fe3faae5edb043f309463b9164
Jaggy Gauran <jaggy@artisan.studio>
2019-04-25 13:38:48 +0800

    ✨ Clean up the method a bit.

--- src/Settings.php
+++ src/Settings.php
@@ -2,6 +2,9 @@
 
 namespace Artisan\Settings;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Fluent;
+
 class Settings
 {
     protected $defaults = [];
@@ -30,6 +33,21 @@ class Settings
             ->model
             ->properties()
             ->where('key', $attribute)
-            ->value('value') ?? $default;
+            ->value('value') ?? $default ?? Arr::get($this->defaults, $attribute);
+    }
+
+    public function all()
+    {
+        $settings = [];
+
+        collect($this->defaults)->map(function ($value, $key) {
+            return new Fluent(compact('key', 'value'));
+        })->merge(
+            $this->model->properties
+        )->each(function ($property) use (&$settings) {
+            Arr::set($settings, $property->key, $property->value);
+        });
+
+        return $settings;
     }
 }
